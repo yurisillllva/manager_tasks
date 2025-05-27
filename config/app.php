ef1de8e91eb562017d372c16e73584511b39ad0c
Yuri Silva <ysm281@gmail.com>
2025-05-27 04:01:22 -0300

    adjustments in providers

--- config/app.php
+++ config/app.php
@@ -141,7 +141,7 @@ return [
     Illuminate\Notifications\NotificationServiceProvider::class,
     Illuminate\Pagination\PaginationServiceProvider::class,
     Illuminate\Pipeline\PipelineServiceProvider::class,
-    Illuminate\Queue\QueueServiceProvider::class, // ✅ Necessário pro queue:listen
+    Illuminate\Queue\QueueServiceProvider::class, 
     Illuminate\Redis\RedisServiceProvider::class,
     Illuminate\Auth\Passwords\PasswordResetServiceProvider::class,
     Illuminate\Session\SessionServiceProvider::class,
@@ -161,6 +161,6 @@ return [
     // App\Providers\AuthServiceProvider::class,
     // App\Providers\EventServiceProvider::class,
     App\Providers\RouteServiceProvider::class,
-    App\Providers\RepositoryServiceProvider::class, // Se você criou esse
+    App\Providers\RepositoryServiceProvider::class,
     ],
 ];
