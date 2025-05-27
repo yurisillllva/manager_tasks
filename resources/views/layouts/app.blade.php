563b742f33bf2f660cc29abeb5817b6685a53cae
Yuri Silva <ysm281@gmail.com>
2025-05-27 03:19:10 -0300

    created view layout

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Task Manager</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body class="bg-gray-100">
+    <div class="container mx-auto px-4 py-8">
+        @yield('content')
+    </div>
+</body>
+</html>
