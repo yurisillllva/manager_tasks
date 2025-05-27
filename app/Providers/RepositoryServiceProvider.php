fcde312b5a57faddcae214614b10944468afed82
Yuri Silva <ysm281@gmail.com>
2025-05-27 03:16:09 -0300

    created repository service provider

---
+++ app/Providers/RepositoryServiceProvider.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Providers;
+
+use App\Interfaces\TaskRepositoryInterface;
+use App\Repositories\TaskRepository;
+use Illuminate\Support\ServiceProvider;
+
+class RepositoryServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register()
+    {
+        $this->app->bind(TaskRepositoryInterface::class, TaskRepository::class);
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        //
+    }
+}
