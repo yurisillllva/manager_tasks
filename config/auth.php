bd58ab635ed721c2adedc7c28cf54721478cf3c7
Yuri Silva <ysm281@gmail.com>
2025-05-27 03:11:07 -0300

    config auth added

--- config/auth.php
+++ config/auth.php
@@ -36,9 +36,15 @@ return [
     */
 
     'guards' => [
+        'web' => [
+            'driver' => 'session',
+            'provider' => 'users',
+        ],
+
         'api' => [
             'driver' => 'jwt',
             'provider' => 'users',
+            'hash' => false,
         ],
     ],
 
