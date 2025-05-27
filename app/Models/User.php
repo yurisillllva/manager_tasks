94c68df230e1c8b5b14fa34e507860f9805e7375
Yuri Silva <ysm281@gmail.com>
2025-05-27 07:26:53 -0300

    added hash in password

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,6 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Tymon\JWTAuth\Contracts\JWTSubject;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Support\Facades\Hash;
 
 class User extends Authenticatable implements JWTSubject
 {
@@ -20,6 +22,13 @@ class User extends Authenticatable implements JWTSubject
         'password',
         'remember_token',
     ];
+
+    protected function password(): Attribute
+{
+    return Attribute::make(
+        set: fn(string $value) => Hash::make($value) // Corrigir para usar o valor recebido
+    );
+}
 
     public function getJWTIdentifier()
     {
