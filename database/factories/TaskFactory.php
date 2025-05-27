bdbbeffa72abb7f9bb492676e92272b1bc7b0ab2
Yuri Silva <ysm281@gmail.com>
2025-05-27 03:13:29 -0300

    created task factories

---
+++ database/factories/TaskFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Task;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Task>
+ */
+class TaskFactory extends Factory
+{
+    protected $model = Task::class;
+
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->sentence,
+            'description' => $this->faker->paragraph,
+            'status' => $this->faker->randomElement(['pendente', 'em andamento', 'concluída']),
+            'user_id' => \App\Models\User::factory(),
+        ];
+    }
+}
