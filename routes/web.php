6d087e65ad6e47d3dbe6a2d8c37032a218c8ec2d
Yuri Silva <ysm281@gmail.com>
2025-05-27 03:17:34 -0300

    created routes project

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,22 @@
 <?php
 
+use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
+// Rotas Públicas (Web)
 Route::get('/', function () {
     return view('welcome');
+});
+
+// Rotas Protegidas (Web)
+Route::middleware(['auth'])->group(function () { // Use 'auth' padrão para web
+    Route::resource('tasks', TaskController::class);
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
+
+// Rotas de Autenticação (API)
+Route::prefix('api')->group(function () {
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/login', [AuthController::class, 'login']);
 });
